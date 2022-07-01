115f81cb0f53f2f6f8dacf78c992b77cc3a673dc
Sebastian <s.albertogamarro5@gmail.com>
2022-06-30 22:54:47 -0600

    html set-up

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT  || 5000;
 
-app.use("/", express.static(path.join(__dirname, '../public')) )
+app.use("/", express.static(path.join(__dirname, '../public')));
 
 app.listen(PORT, (req,res)=>{
     console.log("Server is working on port" + PORT)
